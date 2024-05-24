ce58200c9053f13b503a32243530e085f43a65ba
cybernox <francesco.nocentini.92@gmail.com>
2024-05-24 08:19:19 +0200

    Fixed warnings

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def printHandshakes(peoples):
     handshakes = 0
     # First represent the first people involved in the handshake
     # Cycling all the list
-    for first in range(0,len(peoples)-1):
+    for first in range(0, len(peoples)-1):
         # Second represent the first people involved in the handshake
         # Cycling all the list without the elements already considered
-        for second in range(first+1,len(peoples)):
+        for second in range(first+1, len(peoples)):
             print(peoples[first] + " shakes hands with " + peoples[second])
-            handshakes+=1
+            handshakes += 1
     return handshakes
+
 
 assert printHandshakes(['Alice', 'Bob']) == 1
 assert printHandshakes(['Alice', 'Bob', 'Carol']) == 3
